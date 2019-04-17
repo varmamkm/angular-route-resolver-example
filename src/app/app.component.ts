1b681278de5f447df0d60759e327d19b2ad21304
varmamkm <varmamkm@gmail.com>
2019-04-17 11:48:34 +0200

    4 files modified, 9 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  name = 'Friends';
+
+  constructor() { }
+
 }
